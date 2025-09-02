444fd7e8b5aac269f563ddc2b4004634696bb9a2
RayhanaAkterDev <rayhanaaktersumaya.dev@gmail.com>
2025-09-03 00:02:50 +0600

    setup(frontend): initial project setup with react, tailwind, router, icons

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-lg font-bold">
+        Hospital Management
+        System
+      </h1>
+    </>
+  );
+}
+
+export default App;
